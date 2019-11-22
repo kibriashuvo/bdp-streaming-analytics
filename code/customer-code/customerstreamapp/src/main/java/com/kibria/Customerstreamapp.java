c4a39bf0ceb607e3d504e418462691b949447624
Shuvo Kibria <kibria.shuvo@aalto.fi>
2019-11-22 02:04:16 +0200

    Sliding window calculation done

--- code/customer-code/customerstreamapp/src/main/java/com/kibria/Customerstreamapp.java
+++ code/customer-code/customerstreamapp/src/main/java/com/kibria/Customerstreamapp.java
@@ -1,15 +1,18 @@
 package com.kibria;
 
-
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
 import java.util.Properties;
 
 
@@ -28,61 +31,86 @@ public class Customerstreamapp {
 		}
 	}
 
-  public static void main(String[] args) throws Exception {
-
-    // create execution environment
-	StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-	
-
-	//Setting to work with EventTime
-	env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-    
-    Properties properties = new Properties();
-    
-    properties.setProperty("bootstrap.servers", "localhost:9092");
-    // only required for Kafka 0.8
-    properties.setProperty("zookeeper.connect", "localhost:2181");
-	properties.setProperty("group.id", "test");
-	
-    
-
-	// parse user parameters
-	//ParameterTool parameterTool = ParameterTool.fromArgs(args);
-
-	DataStream<TaxiRideEvent> messageStream = env.addSource(new FlinkKafkaConsumer<>("mytopic", new TaxiRideSerializer(), properties));
-
-
-	//Assigning timestamp to each event 
-	DataStream<TaxiRideEvent> msgStreamWithTSandWM = messageStream.assignTimestampsAndWatermarks(new MyExtractor());
-
-	DataStream<Tuple2<Integer,String>> tipByDestination = msgStreamWithTSandWM.
-															keyBy(r -> r.getDOLocationID()).
-															window(SlidingEventTimeWindows.of(Time.minutes(5),Time.minutes(1))).
-															process(new CalculateTipAmount());
-
-	//DataStream<Tuple2<Integer,String>> maxTipDest = tipByDestination.
-	//												timeWindowAll(Time.hours(1)).maxBy(2);
-	
-
-	// print() will write the contents of the stream to the TaskManager's standard out stream
-	// the rebelance call is causing a repartitioning of the data so that all machines
-	// see the messages (for example in cases when "num kafka partitions" < "num flink operators"
-	/*
-	messageStream.rebalance().map(new MapFunction<TaxiRideEvent, String>() {
-		private static final long serialVersionUID = -6867736771747690202L;
-
-		@Override
-		public String map(TaxiRideEvent event) throws Exception {
-			return "Kafka and Flink says: " + event.getStore_and_fwd_flag();
+	private static class ReduceBySummingTip implements ReduceFunction<TaxiRideEvent> {
+		public TaxiRideEvent reduce(TaxiRideEvent r1, TaxiRideEvent r2) {
+			//Combining the tip and setting it to r1 and then returning r1
+			r1.setTip_amount(r1.getTip_amount() + r2.getTip_amount());
+		  	return r1;
 		}
-	}).print();
-	*/
-	tipByDestination.print();
+	  }
+	  
+	  private static class TotalTipForThisWindow extends ProcessWindowFunction<
+		TaxiRideEvent, Tuple2<Integer, String>, Integer, TimeWindow> {
+	  
+		@Override
+		public void process(
+		  Integer key,
+		  Context context,
+		  Iterable<TaxiRideEvent> summedTrip,
+		  Collector<Tuple2<Integer, String>> out) {
+			  	  
+		  TaxiRideEvent sum = summedTrip.iterator().next();
+		  out.collect(new Tuple2<>(key,"Total tip for this location = "+sum.getTip_amount()));
+		}
+	  }
+
+	public static void main(String[] args) throws Exception {
+
+		// create execution environment
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		
+
+		//Setting to work with EventTime
+		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+		
+		Properties properties = new Properties();
+		
+		properties.setProperty("bootstrap.servers", "localhost:9092");
+		// only required for Kafka 0.8
+		properties.setProperty("zookeeper.connect", "localhost:2181");
+		properties.setProperty("group.id", "test");
+		
+		
+
+		// parse user parameters
+		//ParameterTool parameterTool = ParameterTool.fromArgs(args);
+
+		DataStream<TaxiRideEvent> messageStream = env.addSource(new FlinkKafkaConsumer<>("mytopic", new TaxiRideSerializer(), properties));
+
+
+		//Assigning timestamp to each event 
+		DataStream<TaxiRideEvent> msgStreamWithTSandWM = messageStream.assignTimestampsAndWatermarks(new MyExtractor());
+
+		DataStream<Tuple2<Integer,String>> tipByDestination = msgStreamWithTSandWM.
+																keyBy(r -> r.getDOLocationID()).
+																window(SlidingEventTimeWindows.of(Time.hours(1),Time.minutes(30))).
+																reduce(new ReduceBySummingTip(),new TotalTipForThisWindow());
+
+		//DataStream<Tuple2<Integer,String>> maxTipDest = tipByDestination.
+		//												timeWindowAll(Time.hours(1)).maxBy(2);
+		
+
+		// print() will write the contents of the stream to the TaskManager's standard out stream
+		// the rebelance call is causing a repartitioning of the data so that all machines
+		// see the messages (for example in cases when "num kafka partitions" < "num flink operators"
+		/*
+		messageStream.rebalance().map(new MapFunction<TaxiRideEvent, String>() {
+			private static final long serialVersionUID = -6867736771747690202L;
+
+			@Override
+			public String map(TaxiRideEvent event) throws Exception {
+				return "Kafka and Flink says: " + event.getStore_and_fwd_flag();
+			}
+		}).print();
+		*/
+		tipByDestination.print();
+		
+		//maxTipDest.print();
+		env.execute();
+
+		
+
+
 	
-	//maxTipDest.print();
-	env.execute();
-
-
-   
-  }
+	}
 }
--- reports/Assignment-3-Design.md
+++ reports/Assignment-3-Design.md
@@ -5,3 +5,10 @@ it is a free form. you can add:
 * your design
 * your test result
 * etc.
+
+
+I want to compute max tips per 15 mins computed in every 5 mins, keyed by location id
+
+
+
+Incremental Aggegation to be more efficient, rather than buffering up events in a window
